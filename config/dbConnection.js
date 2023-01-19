090a0a44f9b71d005ccceb4965179d9b4b412165
asfand687 <asfandyar687@gmail.com>
2023-01-20 01:10:49 +0500

    added mongoose and database connection file

---
+++ config/dbConnection.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+
+// INITIATE THE MONGODB CONNECTION
+const dbConnection = async () => {
+
+    try {
+        const dbOptions = {
+            dbName: `ecommerce-store`,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+        await mongoose.connect(process.env.MONGO_URI, dbOptions, (err) => {
+            if (err) {
+                console.log(err.message);
+                return console.log("DB FAILED TO CONNECT :(");
+            }
+            // DB CONNECTED 
+            console.log('DB CONNECTED 100%');
+        })
+    } catch (err) {
+        console.log(err.message);
+        process.exit(1);
+    }
+}
+
+module.exports = dbConnection;
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-require('dotenv').config();
 const express = require('express');
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const dbConnection = require('./config/dbConnection');
+
+dbConnection()
 
 
 // Middlewares
