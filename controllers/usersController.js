1efd9000f860f7c3a58d3b58b6c5ccfc5903f248
asfand687 <asfandyar687@gmail.com>
2023-01-30 19:52:34 +0500

    added get all users function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,12 @@
 const User = require('../models/User')
 
 export const getAllUsers = async (req, res) => {
-
+  const query = req.query.new
   try {
-    console.log("Get All Users")
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find()
+    res.status(200).json(users)
   } catch (error) {
     res.status(500).json(err)
   }
