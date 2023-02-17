d06d89c2490346be070ae0c657712f9332cc9ab2
asfand687 <asfandyar687@gmail.com>
2023-02-17 22:48:27 +0500

    added delete user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -11,3 +11,36 @@ export const getAllUsers = async (req, res) => {
     res.status(500).json(err)
   }
 } 
+
+//UPDATE
+export const updateUser = async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+// DELETE USER
+export const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
