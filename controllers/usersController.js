01164313210c77e06b66d43171a94f82adff0311
asfand687 <asfandyar687@gmail.com>
2023-02-17 22:49:18 +0500

    added get single user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -44,3 +44,14 @@ export const deleteUser = async (req, res) => {
     res.status(500).json(err);
   }
 }
+
+// GET SINGLE USER
+async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
