b2e5e0f172586dc74405307734eab4d35a2492c7
asfand687 <asfandyar687@gmail.com>
2023-02-17 22:50:17 +0500

    added get single user

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 const router = require("express").Router()
-const { getAllUsers, updateUser, deleteUser } = require("../controllers/usersController")
+const { getAllUsers, updateUser, deleteUser, getSingleUser } = require("../controllers/usersController")
 const { verifyTokenAndAuthorization } = require("../utils/verifyToken")
 
 // Get All Users
 router.get("/", getAllUsers)
+
+// Get All Users
+router.get("/:id", getSingleUser)
 
 // Update User
 router.put("/:id", verifyTokenAndAuthorization, updateUser)
