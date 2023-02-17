100a886e95e648564b88b88356a9e128c12484c9
asfand687 <asfandyar687@gmail.com>
2023-02-17 22:52:52 +0500

    added get user stats

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const { getAllUsers, updateUser, deleteUser, getSingleUser } = require("../controllers/usersController")
+const { getAllUsers, updateUser, deleteUser, getSingleUser, getUserStats } = require("../controllers/usersController")
 const { verifyTokenAndAuthorization } = require("../utils/verifyToken")
 
 // Get All Users
@@ -8,8 +8,12 @@ router.get("/", getAllUsers)
 // Get All Users
 router.get("/:id", getSingleUser)
 
+// Get User Stats
+router.get("/stats", getUserStats)
+
 // Update User
 router.put("/:id", verifyTokenAndAuthorization, updateUser)
 
 // Delete User
 router.delete("/:id", verifyTokenAndAuthorization, deleteUser)
+
