a22d6610b33ba6a4d5b892d9e73f3cb0c26862ff
asfand687 <asfandyar687@gmail.com>
2023-02-17 22:47:05 +0500

    added update user route

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,12 @@
 const router = require("express").Router()
-const { getAllUsers } = require("../controllers/usersController")
+const { getAllUsers, updateUser, deleteUser } = require("../controllers/usersController")
+const { verifyTokenAndAuthorization } = require("../utils/verifyToken")
 
 // Get All Users
 router.get("/", getAllUsers)
+
+// Update User
+router.put("/:id", verifyTokenAndAuthorization, updateUser)
+
+// Delete User
+router.delete("/:id", verifyTokenAndAuthorization, deleteUser)
