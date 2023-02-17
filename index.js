2135969831192f97e4577f3432cc928390d24f76
asfand687 <asfandyar687@gmail.com>
2023-02-17 22:58:57 +0500

    added user route middleware

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const dbConnection = require('./config/dbConnection');
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
 
 dbConnection()
 
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: false }))
 
 // Routes
 app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
 
 app.get('/', (req, res) => {
   res.send(`Welcome to the Home Route! Running on PORT:${PORT}`);
